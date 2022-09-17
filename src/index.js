fdf379eaf6a065754766bc2622159003df2bab8b
MinhHieu259 <mhieu7252@gmail.com>
2022-09-17 23:32:06 +0700

    df

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const port = 3000
 
 // static resource
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 // HTTP Logger
 app.use(morgan('combined'))
@@ -20,11 +24,20 @@ app.set('views', path.join(__dirname, 'resources/views'))
 
 app.get('/', (req, res) => {
   res.render('home')
-})
+});
 
 app.get('/news', (req, res) => {
   res.render('news')
-})
+});
+
+app.get('/search', (req, res) => {
+  res.render('search')
+});
+
+app.post('/search', (req, res) => {
+  console.log(req.body)
+  res.send('')
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
